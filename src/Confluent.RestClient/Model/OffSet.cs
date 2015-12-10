4c8885dfe4b57e21a5565459c5164536943ed4f3
bschmidtbauer <brent.schmidtbauer@gmail.com>
2015-12-10 09:52:35 -0600

    support failed publish response

--- src/Confluent.RestClient/Model/OffSet.cs
+++ src/Confluent.RestClient/Model/OffSet.cs
@@ -8,13 +8,13 @@ namespace Confluent.RestClient.Model
         /// Partition the message was published to, or null if publishing the message failed
         /// </summary>
         [JsonProperty(PropertyName = "partition")]
-        public int PartitionId { get; set; }
+        public int? PartitionId { get; set; }
 
         /// <summary>
         /// Offset of the message, or null if publishing the message failed
         /// </summary>
         [JsonProperty(PropertyName = "offset")]
-        public long Offset { get; set; }
+        public long? Offset { get; set; }
 
         /// <summary>
         /// An error code classifying the reason this operation failed, or null if it succeeded.
@@ -23,5 +23,11 @@ namespace Confluent.RestClient.Model
         /// </summary>
         [JsonProperty(PropertyName = "error_code")]
         public long? ErrorCode { get; set; }
+
+        /// <summary>
+        /// An error message describing the reason this operation failed, or null if it succeeded.
+        /// </summary>
+        [JsonProperty(PropertyName = "error")]
+        public string Error { get; set; }
     }
 }
